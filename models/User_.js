1faa8de6bacfb5f17cc97a500f521233eeeaf240
David Cate <david@element-360.com>
2019-01-17 14:57:50 -0500

    heroku filename fix

---
+++ models/User_.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose; // const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+	googleID: String,
+	credits: {
+		type: Number,
+		default: 0
+	}
+});
+
+// this code tells mongoose we want to create a new collection called users
+// when mongoose boots up, if this collection already exits, it will not delete it
+// if will only create it, if it does not exist
+mongoose.model('users', userSchema);
